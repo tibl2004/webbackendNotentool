2b4a1b0662d3c8aa8a9a2dc68369e1a5cc7b3228
Timo Blumer <timo.blumer@gewa.ch>
2024-02-13 23:19:42 +0100

    update deploy

--- routes/faecher.router.js
+++ routes/faecher.router.js
@@ -1,11 +1,10 @@
 const express = require("express");
 const router = express.Router();
-
 const faecherController = require("../controller/faecher.controller");
 
 router.get("/", faecherController.getAll);
 router.get("/:id", faecherController.getById);
-router.get("/:id/pruefungen", faecherController.getPruefungenForFach); // Ändern Sie den Controller-Namen
+router.get("/:id/pruefungen", faecherController.getPruefungenForFach);
 router.post("/", faecherController.create);
 router.put("/:id", faecherController.update);
 router.delete("/:id", faecherController.remove);
