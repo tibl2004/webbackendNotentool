ea6cfaa9ea0c988e5805699f6f32869ecb2ed52b
Timo Blumer <timo.blumer@gewa.ch>
2024-09-01 18:26:59 +0200

    final

--- index.js
+++ index.js
@@ -1,20 +1,33 @@
-const express = require("express");
-const cors = require("cors");
+const express = require('express');
+
+const cors = require('cors');
+const dotenv = require('dotenv');
+
+dotenv.config();
+
 const app = express();
 
-require('dotenv').config();
 
 app.use(cors());
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 
+// Routen importieren
 const educationRouter = require('./routes/education.router');
 
-app.use("/api/education", educationRouter);
 
 
+// Routen zuweisen
+app.use('/api/education', educationRouter);
 
-const PORT = process.env.PORT || 3000; // Port 3000 wird verwendet, falls kein anderer Port angegeben ist
-app.listen(PORT, () => {
-    console.log(`Server is running on port ${PORT}`);
+// Fehlerbehandlung
+app.use((err, req, res, next) => {
+    console.error(err.stack);
+    res.status(500).send('Etwas ist schiefgelaufen!');
+});
+
+// Server starten
+const PORT = process.env.PORT || 5000;
+server.listen(PORT, () => {
+    console.log(`Server läuft auf Port ${PORT}....`);
 });
