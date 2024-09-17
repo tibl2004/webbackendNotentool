e9e33495cd5b59e855a0afb3f234c403772f1787
Timo Blumer <timo.blumer@gewa.ch>
2024-09-17 12:32:44 +0200

    jsfjsf

--- routes/education.router.js
+++ routes/education.router.js
@@ -16,31 +16,25 @@ router.post('/login', educationController.login);
 router.get('/lehrbetriebe', educationController.authenticateToken, educationController.getLehrbetriebe);
 router.get('/lernende', educationController.authenticateToken, educationController.getLernende);
 
-// Routen zu Lernenden mit Noten in den Fächer
-router.get('/lernende/:lernenderId/fach/:fachId/noten', educationController.authenticateToken, educationController.getNotenFuerFach);
+// Routen zu Lernenden mit Noten in den Fächern
+router.get('/lernende/:lernenderId/fach/:fachId/noten', educationController.authenticateToken, educationController.getNotenByLernenderAndFach);
 router.post('/lernende/:lernenderId/fach/:fachId/noten', educationController.authenticateToken, educationController.addNote);
-router.put('/lernende/:lernenderId/fach/:fachId/noten/:noteId', educationController.authenticateToken, educationController.editNote); // Token-Authentifizierung sicherstelleneducationController.editNote // Die Funktion aufrufen);
+router.put('/lernende/:lernenderId/fach/:fachId/noten/:noteId', educationController.authenticateToken, educationController.updateNote); // Die Funktion anpassen
 router.delete('/lernende/:lernenderId/fach/:fachId/noten/:noteId', educationController.authenticateToken, educationController.deleteNote);
 
 // Lernenden aktualisieren (mit Authentifizierung)
-router.put('/lernender/:lernenderId', educationController.authenticateToken, educationController.updateLernender);
+router.put('/lernende/:lernenderId', educationController.authenticateToken, educationController.updateLernender);
 
 // Fach hinzufügen (mit Authentifizierung) und lernenderId in der URL
 router.post('/lernende/:lernenderId/fach', educationController.authenticateToken, educationController.addFach);
 
-// Fach aktualisieren (mit Authentifizierung) und lernenderId in der URL
-router.put('/lernender/:lernenderId/fach/:fachId', educationController.authenticateToken, educationController.updateFach);
-
-
+// Fach aktualisieren (mit Authentifizierung) und fachId in der URL
+router.put('/lernende/:lernenderId/fach/:fachId', educationController.authenticateToken, educationController.updateFach);
 
 // Endpunkt zum Abrufen der Fächer eines Lernenden (ohne Noten)
-router.get('/lernende/:lernenderId/faecher', educationController.getLernendeMitFaecher);
-
+router.get('/lernende/:lernenderId/faecher', educationController.authenticateToken, educationController.getLernendeMitFaecher);
 
 // Route zum Abrufen eines Lernenden anhand der ID
-router.get('/lernende/:id', educationController.getLernenderById);
-
-
-
+router.get('/lernende/:id', educationController.authenticateToken, educationController.getLernenderById);
 
 module.exports = router;
