d8b9099b478482db6e4eecd01dc33b9705a31b55
Timo Blumer <timo.blumer@gewa.ch>
2024-09-18 15:17:36 +0200

    jdyfjs

--- routes/login.router.js
+++ routes/login.router.js
@@ -2,8 +2,6 @@ const express = require('express');
 const router = express.Router();
 const loginController = require('../controller/login.controller');
 
-// Middleware zur Authentifizierung
-router.use(loginController.authenticateToken);
 
 // Routen
 router.post('/', loginController.login); // Login für Admin, Berufsbildner, Lernende und Lehrbetrieb
