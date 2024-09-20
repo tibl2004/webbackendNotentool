f2bd9598e0e83ad84ce3fe75bbac5c349f403c08
Timo Blumer <timo.blumer@gewa.ch>
2024-09-20 07:25:42 +0200

    jfjs

--- routes/lernender.router.js
+++ routes/lernender.router.js
@@ -5,7 +5,6 @@ const lernenderController = require('../controller/lernender.controller');
 // Middleware zur Authentifizierung
 router.use(lernenderController.authenticateToken);
 
-router.get('/', lernenderController.getLernende);  // Lernenden abrufen
 router.get('/:id', lernenderController.getLernenderById);  // Lernenden abrufen
 router.put('/:lernenderId', lernenderController.updateLernender);  // Lernenden aktualisieren (Nur Lehrbetrieb oder Berufsbildner)
 router.delete('/:id', lernenderController.deleteLernender);  // Lernenden löschen (Nur Lehrbetrieb oder Berufsbildner)
